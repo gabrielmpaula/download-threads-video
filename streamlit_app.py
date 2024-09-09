692d26b9a88f11f69ec4d985f24dce06a2051375
Gabriel Melo de Paula <gabrielmpaula@gmail.com>
2024-09-08 23:28:54 -0300

    download url

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,6 +32,6 @@ try:
     ## URL do vídeo
     Clique no link abaixo para fazer download do video.
     ''')
-    st.html(f'<a href="{video_url}" download>Download</a>')
+    st.html(f"<a download='video.mp4', href='{video_url}' download>Download</a>")
 except:
     st.write('Insira o link que deseja extrair o video.')
